a1e5c6713dfbdb7e3ee99f6e73ae7982edfea31e
Helen <helenlam90@gmail.com>
2015-03-26 13:01:05 -0700

    init

---
+++ db.js
@@ -0,0 +1,24 @@
+
+	var entrees = [
+		{
+			"name" : "Fried Chicken Waffle",
+			"price" : 12.00,
+			"ingredients": ["chicken", "waffle"]
+		},
+		{
+			"name" : "California Roll",
+			"price" : 25.00,
+			"ingredients": ["avocado", "rice", "immitation crab", "wasabi"]
+		},
+		{
+			"name" : "Prime Rib Steak",
+			"price" : 25.00,
+			"ingredients": ["beef", "onion", "bell pepper", "mushroom"]
+		},
+		{
+			"name" : "Lamb Chop",
+			"price" : 30.00,
+			"ingredients": ["lamb", "rose merry"]
+		}
+		 ];
+
