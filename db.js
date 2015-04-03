2f9d5c56cc05adc59708e2049be23b44024ab6ae
Cindy Lam <cindylam_222@hotmail.com>
2015-04-02 19:53:06 -0700

    JavaScript code for sorting and searching table

--- db.js
+++ db.js
@@ -18,7 +18,32 @@
 		{
 			"name" : "Lamb Chop",
 			"price" : 30.00,
-			"ingredients": ["lamb", "rose merry"]
-		}
+			"ingredients": ["lamb","lamb","rose merry"]
+		},
+		{
+			"name" : "Banana Scone",
+			"price" : 5.00,
+			"ingredients": ["banana", "milk","butter","sugar"]
+		},
+		{
+			"name" : "Filet Minon",
+			"price" : 30.00,
+			"ingredients": ["beef", "rose merry","red wine"]
+		},
+		{
+			"name" : "Banana Walnut Bread",
+			"price" : 5.00,
+			"ingredients": ["banana", "walnut","flour"]
+		},
+		{
+			"name" : "Chicken Bacon Ranch",
+			"price" : 8.00,
+			"ingredients": ["chicken", "bread","bacon"]
+		},
+		{
+			"name" : "Lobster Fried Rice",
+			"price" : 32.00,
+			"ingredients": ["lobster", "rice","garlic","onion"]
+		},
 		 ];
 
