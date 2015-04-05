559d87f747d999b65681812bd3d3b225c2562ac4
Cindy Lam <cindylam_222@hotmail.com>
2015-04-05 01:01:38 -0700

    Updated database in db.js, and added new CSS feature to table in index file

--- db.js
+++ db.js
@@ -3,47 +3,56 @@
 		{
 			"name" : "Fried Chicken Waffle",
 			"price" : 12.00,
-			"ingredients": ["chicken", "waffle"]
+			"ingredients": ["chicken", "waffle"],
+			"category": "main"
 		},
 		{
 			"name" : "California Roll",
 			"price" : 25.00,
-			"ingredients": ["avocado", "rice", "immitation crab", "wasabi"]
+			"ingredients": ["avocado", "rice", "immitation crab", "wasabi"],
+			"category": "appetizer"
 		},
 		{
 			"name" : "Prime Rib Steak",
 			"price" : 25.00,
-			"ingredients": ["beef", "onion", "bell pepper", "mushroom"]
+			"ingredients": ["beef", "onion", "bell pepper", "mushroom"],
+			"category": "main"
 		},
 		{
 			"name" : "Lamb Chop",
 			"price" : 30.00,
-			"ingredients": ["lamb","lamb","rose merry"]
+			"ingredients": ["lamb","lamb","rose merry"],
+			"category": "main"
 		},
 		{
 			"name" : "Banana Scone",
 			"price" : 5.00,
-			"ingredients": ["banana", "milk","butter","sugar"]
+			"ingredients": ["banana", "milk","butter","sugar"],
+			"category": "dessert"
 		},
 		{
 			"name" : "Filet Minon",
 			"price" : 30.00,
-			"ingredients": ["beef", "rose merry","red wine"]
+			"ingredients": ["beef", "rose merry","red wine"],
+			"category": "main"
 		},
 		{
 			"name" : "Banana Walnut Bread",
 			"price" : 5.00,
-			"ingredients": ["banana", "walnut","flour"]
+			"ingredients": ["banana", "walnut","flour"],
+			"category": "dessert"
 		},
 		{
 			"name" : "Chicken Bacon Ranch",
 			"price" : 8.00,
-			"ingredients": ["chicken", "bread","bacon"]
+			"ingredients": ["chicken", "bread","bacon"],
+			"category": "main"
 		},
 		{
 			"name" : "Lobster Fried Rice",
 			"price" : 32.00,
-			"ingredients": ["lobster", "rice","garlic","onion"]
+			"ingredients": ["lobster", "rice","garlic","onion"],
+			"category": "main"
 		},
 		 ];
 
